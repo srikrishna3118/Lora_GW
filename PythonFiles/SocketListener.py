8d176e44dc37dcd495748aec72c3f96d138031fb
srikrishna3118 <srikrishna3118@gmail.com>
2016-12-05 15:05:07 +0530

    ugly fix for water data

--- PythonFiles/SocketListener.py
+++ PythonFiles/SocketListener.py
@@ -3,19 +3,22 @@ _author_ = 'Srikrishna'
 '''
     Simple socket server
 '''
+
 import HttpService
 
 import socket
 import sys
 import thread
 import logging
-logger = logging.getLogger(__name__)
 import json
 import re
+import math
+
+logger = logging.getLogger(__name__)
 
 SENS = {
     "ENERGY":{
-        "D0":"4",
+        "D0":"9",
         "D1":"voltage",
         "D2":"current",
         "D3":"frequency",
@@ -31,14 +34,14 @@ SENS = {
         "D1":"water_level",
         "D2":"water_temperature"
     },
-    "POLLUTION":{
+    "ENV":{
         "D0":"5",
-        "D1":"128",
-        "D2":"256",
-        "D3":"512",
-        "D4":"1024",
-        "D5":"2048",
-        "D6":"4096"
+        "D1":"no2",
+        "D2":"co2",
+        "D3":"outside_temperature",
+        "D4":"humidity",
+        "D5":"pressure",
+        "D6":"noise"
     }
 }
 
@@ -61,20 +64,51 @@ class SocketServer(object):
             if cdata:
                 data_parse = json.loads(cdata.groups(1)[0])
 
-                sen_values = SENS[data_parse['type']]["D0"]
-                count = 1
-                while count <= int(sen_values):
-                    str_sen = "D"+str(count)
-                    print str_sen
-                    data_json = {"observations": [{
-                        "value": data_parse[str_sen]}
-                    ]}
-                    print data_json
-                    count = count+1
-                    self.http.putRequest(SENS[data_parse['type']][str_sen], data_json)
+                sensor_type = data_parse['type'].upper()
 
-                for val in data_parse:
-                    print(val)
+                sen_values = SENS[sensor_type]["D0"]
+
+                count = 1
+                if(sensor_type=="WATER"):
+                    logger.info("Dirty hack for now need to figure out a way to handle formulae for each sensors.")
+
+                    V = 3.3* int(data_parse['D2'])/16384
+
+                    R = V*10000/(3.3 - V)
+                    TF = (math.log10(R/10000)/3950)+(1/298.15)
+                    Temperature = (1/TF)-273.15
+                    data_json = {"observations": [{
+                        "value": Temperature}
+                    ]}
+
+                    self.http.putRequest(SENS[sensor_type]['D2'],data_json)
+                    print data_json
+
+                    V = 20.0457 *(math.sqrt( 273.15 + Temperature ))
+
+                    Level = (int(data_parse['D1']) * V * 100)/400000
+
+                    data_json = {"observations": [{
+                        "value": Level}
+                    ]}
+                    self.http.putRequest(SENS[sensor_type]['D1'], data_json)
+                    print data_json
+
+                else:
+                    while count <= int(sen_values):
+                        str_sen = "D"+str(count)
+                        print str_sen
+                        data_json = {"observations": [{
+                            "value": data_parse[str_sen]}
+                        ]}
+                        print data_json
+                        count = count+1
+
+                        #Http request is sent
+                        self.http.putRequest(SENS[sensor_type][str_sen], data_json)
+
+                #for val in data_parse:
+                #    print(val)
 
             else:
                 logger.info("Data cannot be parsed")
